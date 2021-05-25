f7380aecd0bdc6eb611d2ba5156020094c2e41c3
Lukas Bang Stoltz-Andersen <lukasbangstoltz@gmail.com>
2021-05-25 21:56:20 +0200

    linreg er mere eller mindre færdig

--- src/webscraping/residence.py
+++ src/webscraping/residence.py
@@ -1,8 +1,11 @@
 class Residence:
-    def __init__(self, house_type,   house_zip_code, house_rooms, house_square_meters,  house_year, house_price):
+    def __init__(self, house_type,   house_zip_code, house_rooms, house_square_meters,  house_year, house_taxes, house_energy, house_ground_area, house_price):
         self.house_type = house_type
         self.house_price = house_price
         self.house_rooms = house_rooms
         self.house_square_meters = house_square_meters
         self.house_year = house_year
         self.house_zip_code = house_zip_code
+        self.house_taxes = house_taxes
+        self.house_energy = house_energy
+        self.house_ground_area = house_ground_area
